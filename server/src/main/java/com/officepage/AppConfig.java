daae9295a03114f0888d050bbb83e1470ead7f52
xuwushun <1471132931@qq.com>
2017-09-09 16:16:53 +0800

    add

--- server/src/main/java/com/officepage/AppConfig.java
+++ server/src/main/java/com/officepage/AppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.io.BufferedReader;
 import java.io.IOException;
